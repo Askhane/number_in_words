e467567ffd1c1c8424ab76ad3548d1487a0a11d7
Jasmine Duchon <jasmine.duchon@free.fr>
2025-02-20 14:44:52 +0100

    Init project

---
+++ src/test/java/fr/arolla/kata/numberinwords/NumbersInWordsTest.java
@@ -0,0 +1,14 @@
+package fr.arolla.kata.numberinwords;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class NumbersInWordsTest {
+
+    @Test
+    public void shouldWriteZeroFor0() {
+        fail("Not implemented");
+    }
+
+}
